3ac08b6ef44978f2a82e47e38954df041c80b4c1
Victor Fontecha <vfontechac@gmail.com>
2026-01-10 00:28:54 +0100

    add: productController F3.5 finish

---
+++ FrikiTrader.API/Controllers/ProductsController.cs
@@ -0,0 +1,78 @@
+﻿using FrikiTrader.Aplication.DTOs;
+using FrikiTrader.Aplication.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace FrikiTrader.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] ProductCreateDto dto)
+        {
+            //Extraer el id del usuario desde los Claims del token
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+            if (userIdClaim == null)
+            {
+                return Unauthorized(new { message = "Usuario no autenticado." });
+            }
+            int userId = int.Parse(userIdClaim.Value);
+
+            var product = await _productService.CreateAsync(dto, userId);
+            return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _productService.GetAllAsync();
+            return Ok(products);
+
+        }
+
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound(new { message = "Producto no encontrado." });
+            }
+            return Ok(product);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] ProductCreateDto dto)
+        {
+            var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
+            var success = await _productService.UpdateAsync(id, dto, userId);
+            if (!success) return Forbid();
+
+            return NoContent();
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
+            var success = await _productService.DeleteAsync(id, userId);
+            if (!success) return Forbid();
+            return NoContent();
+        }
+
+    }
+}
