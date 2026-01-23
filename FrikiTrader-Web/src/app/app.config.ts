6833bce91a6686b4338b24ba8a3d79d4f2f1b155
Victor Fontecha <vfontechac@gmail.com>
2026-01-23 13:01:06 +0100

    WEB: Firebase instalado y configurado

--- FrikiTrader-Web/src/app/app.config.ts
+++ FrikiTrader-Web/src/app/app.config.ts
@@ -3,6 +3,18 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmEW3kK8SYQp1g4vJUI7xXC6WwwpxXS_o",
+  authDomain: "frikitrader-78a99.firebaseapp.com",
+  projectId: "frikitrader-78a99",
+  storageBucket: "frikitrader-78a99.firebasestorage.app",
+  messagingSenderId: "561456214378",
+  appId: "1:561456214378:web:c19e09a34d29ecdef7f513",
+  measurementId: "G-72Y4V0CD8F"
+};
 
 
 export const appConfig: ApplicationConfig = {
@@ -10,6 +22,8 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideStorage(() => getStorage()),
   ]
 };
